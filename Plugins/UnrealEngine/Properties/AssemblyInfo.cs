d068a743bddf5a7e0d661dd06c247341920a0829
SillyBits <sillybits@onlinehome.de>
2019-07-27 20:21:20 +0200

    First plugin: Unreal engine - Only a single type for now: FString with its hash following

---
+++ Plugins/UnrealEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("UnrealEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SillyBits")]
+[assembly: AssemblyProduct("UnrealEngine plugin for Hexalyzer")]
+[assembly: AssemblyCopyright("Copyright © 2019 SillyBits")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyInformationalVersion("v0.1 alpha")]

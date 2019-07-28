bcac320f45b05372db1ef3fc3aa567bf4c83fc77
SillyBits <sillybits@onlinehome.de>
2019-07-28 13:01:54 +0200

    Analyzer.cs: - Migrated to IAccessor - Added new methods 'IsAsciiChar' and 'IsWideChar' - Adjusted 'Is*String' to check using 'Is*Char' instead of hardcoded checks.

--- Hexalyzer/Helper/Analyzer.cs
+++ Hexalyzer/Helper/Analyzer.cs
@@ -40,123 +40,150 @@ namespace Hexalyzer.Helper
 		/// <param name="maxlen">Maximum length of check (default=8)</param>
 		/// <param name="sign">Which type to check: -1:signed, 0:both, +1:unsigned</param>
 		/// <returns>Type discovered, or null if no match found</returns>
-		public static Type IsOfValue(IReadOnlyList<byte> data, long offset, long value, int minlen = 1, int maxlen = 8, int sign = 0)
+		public static Type IsOfValue(IAccessor<byte> data, long offset, long value, int minlen = 1, int maxlen = 8, int sign = 0)
 		{
-			if (minlen <= 8 && maxlen >= 8 && offset + 8 <= data.Count)
+			if (minlen <= 8 && maxlen >= 8 && offset + 8 <= data.LongCount)
 			{
-				if (sign >= 0 && Helpers.ToUInt64(data, (int)offset) == (ulong)value)
+				if (sign >= 0 && Helpers.ToUInt64(data, offset) == (ulong)value)
 					return typeof(ulong);
 
-				if (sign <= 0 && Helpers.ToInt64(data, (int)offset) == value)
+				if (sign <= 0 && Helpers.ToInt64(data, offset) == value)
 					return typeof(long);
 			}
 
-			if (minlen <= 4 && maxlen >= 4 && value <= uint.MaxValue && offset + 4 <= data.Count)
+			if (minlen <= 4 && maxlen >= 4 && value <= uint.MaxValue && offset + 4 <= data.LongCount)
 			{
-				if (sign >= 0 && Helpers.ToUInt32(data, (int)offset) == value)
+				if (sign >= 0 && Helpers.ToUInt32(data, offset) == value)
 					return typeof(uint);
 
 				if (sign <= 0 && value <= int.MaxValue)
-					if (Helpers.ToInt32(data, (int)offset) == value)
+					if (Helpers.ToInt32(data, offset) == value)
 						return typeof(int);
 			}
 
-			if (minlen <= 2 && maxlen >= 2 && value < ushort.MaxValue && offset + 2 <= data.Count)
+			if (minlen <= 2 && maxlen >= 2 && value < ushort.MaxValue && offset + 2 <= data.LongCount)
 			{
-				if (sign >= 0 && Helpers.ToUInt16(data, (int)offset) == value)
+				if (sign >= 0 && Helpers.ToUInt16(data, offset) == value)
 					return typeof(ushort);
 
 				if (sign <= 0 && value <= short.MaxValue)
-					if (Helpers.ToInt16(data, (int)offset) == value)
+					if (Helpers.ToInt16(data, offset) == value)
 						return typeof(short);
 			}
 
-			if (minlen <= 1 && maxlen >= 1 && value < byte.MaxValue && offset + 1 <= data.Count)
+			if (minlen <= 1 && maxlen >= 1 && value < byte.MaxValue && offset + 1 <= data.LongCount)
 			{
-				if (sign >= 0 && Helpers.ToByte(data, (int)offset) == value)
+				if (sign >= 0 && Helpers.ToByte(data, offset) == value)
 					return typeof(byte);
 
 				if (sign <= 0 && value <= sbyte.MaxValue)
-					if (Helpers.ToSByte(data, (int)offset) == value)
+					if (Helpers.ToSByte(data, offset) == value)
 						return typeof(sbyte);
 			}
 
 			return null;
 		}
 
+
 		/// <summary>
-		/// Check for valid 8bit string
+		/// Check for valid 7bit Ascii char
+		/// </summary>
+		/// <param name="data">Value</param>
+		/// <returns>Outcome</returns>
+		public static bool IsAsciiChar(byte data)
+		{
+			if (data < 32 && !_ValidCtrlChars.Contains(data))
+				return false;
+			return (data <= 127);
+		}
+
+		/// <summary>
+		/// Check for valid 7bit Ascii string
 		/// </summary>
 		/// <param name="data">Data to check</param>
 		/// <param name="offset">Where to start in data</param>
 		/// <param name="length">Assumed length of string</param>
 		/// <returns>Outcome</returns>
-		public static bool IsAsciiString(IReadOnlyList<byte> data, long offset, long length)
+		public static bool IsAsciiString(IAccessor<byte> data, long offset, long length)
 		{
-			if (offset + length > data.Count)
+			if (offset + length > data.LongCount)
 				return false;
 
 			// Null-terminator present?
-			if (data[(int)(offset + length - 1)] != 0)
+			if (data[offset + length - 1] != 0)
 				return false;
 
 			for (long idx = offset; idx < offset + length - 1; ++idx)
-			{
-				byte b = data[(int)idx];
-
-				if (b > 127)
+				if (!IsAsciiChar(data[idx]))
 					return false;
-				if (b < 32 && !_ValidCtrlChars.Contains(b))
-					return false;
-			}
 
 			return true;
 		}
 
+
 		/// <summary>
-		/// Check for valid 16bit string
+		/// Check for valid 16bit UCS-2 char
+		/// </summary>
+		/// <param name="data">Value to check</param>
+		/// <returns>Outcome</returns>
+		public static bool IsWideChar(ushort data)
+		{
+			byte b1 = (byte)(data & 0xFF);
+			data >>= 8;
+			byte b2 = (byte)(data & 0xFF);
+
+			return IsWideChar(b1, b2);
+		}
+
+		/// <summary>
+		/// Check for valid 16bit UCS-2 char
+		/// </summary>
+		/// <param name="byte1">1st byte to check (00##)</param>
+		/// <param name="byte2">2nd byte to check (##00)</param>
+		/// <returns>Outcome</returns>
+		public static bool IsWideChar(byte byte1, byte byte2)
+		{
+			if (0xC2 <= byte1 && byte1 <= 0xDF)
+				return (0x80 <= byte2 && byte2 <= 0xBF);
+
+			return (byte2 == 0) && IsAsciiChar(byte1);
+		}
+
+		/// <summary>
+		/// Check for valid 16bit UCS-2 string
 		/// </summary>
 		/// <param name="data">Data to check</param>
 		/// <param name="offset">Where to start in data</param>
 		/// <param name="length">Assumed length of string</param>
 		/// <returns>Outcome</returns>
-		public static bool IsWideString(IReadOnlyList<byte> data, long offset, long length)
+		public static bool IsWideString(IAccessor<byte> data, long offset, long length)
 		{
 			long len2 = length << 1;
 
-			if (offset + len2 > data.Count)
+			if (offset + len2 > data.LongCount)
 				return false;
 
 			// Null-terminator present?
-			if (data[(int)(offset + len2 - 2)] != 0 || data[(int)(offset + len2 - 1)] != 0) 
+			if (data[offset + len2 - 2] != 0 || data[offset + len2 - 1] != 0) 
 				return false;
 
 			for (long idx = offset; idx < offset + len2 - 2; idx += 2)
 			{
-				byte b1 = data[(int) idx     ];
-				byte b2 = data[(int)(idx + 1)];
+				byte b1 = data[idx    ];
+				byte b2 = data[idx + 1];
 
-				if (b1 < 128)
-				{
-					if (b2 != 0)
-						return false;
-					if (b1 < 32 && !_ValidCtrlChars.Contains(b1))
-						return false;
-				}
-				else if (0xC2 <= b1 && b1 <= 0xDF)
-				{
-					if (b2 < 0x80 || b2 > 0xBF)
-						return false;
-				}
+				if (!IsWideChar(b1, b2))
+					return false;
 			}
 
 			return true;
 		}
 
+
 		//TODO: Add UTF-32 checker, but do need some good examples to get implementation right
 
 
-		private static byte[] _ValidCtrlChars = new byte[] { (byte)'\t', (byte)'\n', (byte)'\r' };
+		private static byte[] _ValidCtrlChars = new byte[] { 0, (byte)'\t', (byte)'\n', (byte)'\r' };
 
 	}
 
